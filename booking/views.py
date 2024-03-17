761db95c5835f1f975f1c580fe56b0ca2fa6acf4
Graeme Adamson <graya0277@gmail.com>
2024-03-17 18:01:53 +0000

    added booking template

--- booking/views.py
+++ booking/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def booking(request):
-    return HttpResponse("Hello, Booking!")
+        return render(request, 'booking/booking.html', {})
