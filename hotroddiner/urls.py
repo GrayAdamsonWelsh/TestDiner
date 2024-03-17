17507b46e7a59ea8bfecf1ba508d16240ae8abd6
Graeme Adamson <graya0277@gmail.com>
2024-03-17 14:40:31 +0000

    about displays a message

--- hotroddiner/urls.py
+++ hotroddiner/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from about.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', about, name='about'),
 ]
